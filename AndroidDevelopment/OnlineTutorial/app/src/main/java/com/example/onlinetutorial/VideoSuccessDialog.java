d73209f7e42d9f045ec3e4f52c4f9ac0df7b811d
Paul Byrne <byrnep21@tcd.ie>
2019-02-26 17:34:17 +0000

    Started implementing methods in UploadAndSendActivity.java

--- AndroidDevelopment/OnlineTutorial/app/src/main/java/com/example/onlinetutorial/VideoSuccessDialog.java
+++ AndroidDevelopment/OnlineTutorial/app/src/main/java/com/example/onlinetutorial/VideoSuccessDialog.java
@@ -6,7 +6,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
 
-public class VideoSuccessDialog extends AppCompatDialogFragment {
+public class VideoSuccessDialog extends AppCompatDialogFragment  {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
