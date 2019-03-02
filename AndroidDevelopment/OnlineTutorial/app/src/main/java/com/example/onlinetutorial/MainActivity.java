75d1b43baafcc98fdb8b453890a46bcdb86a2214
Paul Byrne <byrnep21@tcd.ie>
2019-03-02 12:12:52 +0000

    App now asks for camera permission on first launch

--- AndroidDevelopment/OnlineTutorial/app/src/main/java/com/example/onlinetutorial/MainActivity.java
+++ AndroidDevelopment/OnlineTutorial/app/src/main/java/com/example/onlinetutorial/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.onlinetutorial;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +14,18 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private int requestCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = this;
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, requestCode);
+        }
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 openCameraActivity();
             }
         });
+
     }
 
     public void openCameraActivity(){
